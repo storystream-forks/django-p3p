c294c7f96a3efa35ffcd664e87c6c2f232f748fa
storystream-forks <richard.hayes@storystream.it>
2015-01-25 22:40:16 +0000

    bumped version and updated values in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-p3p',
-    version='1.0.1',
-    description='Django P3P makes it easier to set P3P HTTP headers to prevent session loss.',
+    version='1.0.2',
+    description='Fork of http://github.com/jjanssen/django-p3p',
     long_description=open('README.rst').read(),
-    author='Janneke Janssen',
-    author_email='j.janssen@lukkien.com',
+    author='StoryStream',
+    author_email='hello@storystream.it',
     keywords='session patch middleware django',
-    url='http://github.com/jjanssen/django-p3p',
+    url='http://github.com/storystream-forks/django-p3p',
     packages=find_packages(exclude=['p3p_example']),
     include_package_data=True,
     platforms='any',
